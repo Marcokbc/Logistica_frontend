173cce3383502aea2166a79707d72d4899578aee
Marco Antônio Meira de Lima <88397083+Marcokbc@users.noreply.github.com>
2023-05-07 18:21:40 -0300

    GetOrders

--- logistica/src/app/(authenticated)/admin/page.tsx
+++ logistica/src/app/(authenticated)/admin/page.tsx
@@ -9,13 +9,18 @@ import ModalRota from "../components/ModalRota";
 import ModalLogout from "../components/ModalLogout";
 import axios from "axios";
 import api from "@/app/services/api";
+import { Order } from "@/app/models/Order";
+
 
 export default function Admin() {
+    const [pedidos, setPedidos] = useState<Order>();
+    const [pageNumber, setPageNumber] = useState(1);
+    const [pageSize, setPageSize] = useState(5);
     const token = localStorage.getItem('token');
 
     const authorization = {
-        headers : {
-          Authorization : `Bearer ${token}`
+        headers: {
+            Authorization: `Bearer ${token}`
         }
     }
 
@@ -35,23 +40,28 @@ export default function Admin() {
 
     const [orders, setOrders] = useState([]);
 
-    const modalOn = (modalOn : boolean) => {
+    const modalOn = (modalOn: boolean) => {
         setShowModalLogout(modalOn);
     }
 
     const modalOff = (modalOff: boolean) => {
         setShowModalLogout(modalOff);
     }
-   
-    // useEffect( ()=> {
-    //     api.get('',authorization).then(
-    //       response => {setOrders(response.data);
-    //      }, token)
-    //   })
+
+    try {
+        api.get(`api/Pedido?pageNumber=${pageNumber}&pageSize=${pageSize}`, authorization)
+            .then(
+                response => {
+                    setPedidos(response.data.items);
+                    console.log(response.data);
+                });
+    } catch (error) {
+        console.log(error);
+    }
 
     return (
         <>
-            <Header modalOn={modalOn}/>
+            <Header modalOn={modalOn} />
             <main className='bg-gray-100'>
                 <div className="lg:mt-0 lg:flex-shrink-0">
                     <div className="flex flex-col space-y-4 h-screen justify-center items-center">
@@ -92,75 +102,77 @@ export default function Admin() {
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <tr className="text-gray-700">
-                                        <td className="border-b-2 p-4 dark:border-dark-5">1</td>
-                                        <td className="border-b-2 p-4 dark:border-dark-5">Roupas</td>
-                                        <td className="border-b-2 p-4 dark:border-dark-5">Pedido Efetuado</td>
-                                        <td className="border-b-2 p-4 dark:border-dark-5">Jl987</td>
-                                        <td className="border-b-2 p-4 dark:border-dark-5">
-                                            <button
-                                                onClick={() => setShowModalRota(true)}
-                                            >
-                                                <svg
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    width="25"
-                                                    height="25"
-                                                    fill="none"
-                                                    viewBox="0 0 24 24"
+                                    {pedidos?.map(pedido =>
+                                        <tr className="text-gray-700">
+                                            <td className="border-b-2 p-4 dark:border-dark-5">{pedido.id}</td>
+                                            <td className="border-b-2 p-4 dark:border-dark-5">{pedido.nome}</td>
+                                            <td className="border-b-2 p-4 dark:border-dark-5">{pedido.status}</td>
+                                            <td className="border-b-2 p-4 dark:border-dark-5">{pedido.codigoRastreio}</td>
+                                            <td className="border-b-2 p-4 dark:border-dark-5">
+                                                <button
+                                                    onClick={() => setShowModalRota(true)}
                                                 >
-                                                    <g>
-                                                        <path
-                                                            stroke="#000"
-                                                            strokeLinecap="round"
-                                                            strokeLinejoin="round"
-                                                            strokeWidth="2"
-                                                            d="M8 12h4m0 0h4m-4 0v4m0-4V8m0 13a9 9 0 110-18 9 9 0 010 18z"
-                                                        ></path>
-                                                    </g>
-                                                </svg>
-                                            </button>
-                                        </td>
-                                        <td className="border-b-2 p-4 dark:border-dark-5">
-                                            <button
-                                                onClick={() => setShowModalUpdate(true)}
-                                            >
-                                                <svg
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    width="25"
-                                                    height="25"
-                                                    viewBox="0 0 24 24"
+                                                    <svg
+                                                        xmlns="http://www.w3.org/2000/svg"
+                                                        width="25"
+                                                        height="25"
+                                                        fill="none"
+                                                        viewBox="0 0 24 24"
+                                                    >
+                                                        <g>
+                                                            <path
+                                                                stroke="#000"
+                                                                strokeLinecap="round"
+                                                                strokeLinejoin="round"
+                                                                strokeWidth="2"
+                                                                d="M8 12h4m0 0h4m-4 0v4m0-4V8m0 13a9 9 0 110-18 9 9 0 010 18z"
+                                                            ></path>
+                                                        </g>
+                                                    </svg>
+                                                </button>
+                                            </td>
+                                            <td className="border-b-2 p-4 dark:border-dark-5">
+                                                <button
+                                                    onClick={() => setShowModalUpdate(true)}
                                                 >
-                                                    <path d="M20.7 5.2a1.024 1.024 0 010 1.448l-2.626 2.628-3.35-3.35L17.35 3.3a1.024 1.024 0 011.448 0zm-4.166 5.614l-3.35-3.35-8.509 8.511L3 21l5.025-1.675z"></path>
-                                                </svg>
-                                            </button>
-                                        </td>
-                                        <td className="border-b-2 p-4 dark:border-dark-5">
-                                            <button
-                                                type="button"
-                                                data-modal-target="popup-modal"
-                                                data-modal-toggle="popup-modal"
-                                                onClick={() => setShowModalDelete(true)}
-                                            >
-                                                <svg
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    width="25"
-                                                    height="25"
-                                                    fill="none"
-                                                    viewBox="0 0 24 24"
+                                                    <svg
+                                                        xmlns="http://www.w3.org/2000/svg"
+                                                        width="25"
+                                                        height="25"
+                                                        viewBox="0 0 24 24"
+                                                    >
+                                                        <path d="M20.7 5.2a1.024 1.024 0 010 1.448l-2.626 2.628-3.35-3.35L17.35 3.3a1.024 1.024 0 011.448 0zm-4.166 5.614l-3.35-3.35-8.509 8.511L3 21l5.025-1.675z"></path>
+                                                    </svg>
+                                                </button>
+                                            </td>
+                                            <td className="border-b-2 p-4 dark:border-dark-5">
+                                                <button
+                                                    type="button"
+                                                    data-modal-target="popup-modal"
+                                                    data-modal-toggle="popup-modal"
+                                                    onClick={() => setShowModalDelete(true)}
                                                 >
-                                                    <g>
-                                                        <path
-                                                            stroke="#000"
-                                                            strokeLinecap="round"
-                                                            strokeLinejoin="round"
-                                                            strokeWidth="2"
-                                                            d="M14 10v7m-4-7v7M6 6v11.8c0 1.12 0 1.68.218 2.108a2 2 0 00.874.874c.427.218.987.218 2.105.218h5.606c1.118 0 1.677 0 2.104-.218.377-.192.683-.498.875-.874.218-.428.218-.987.218-2.105V6M6 6h2M6 6H4m4 0h8M8 6c0-.932 0-1.398.152-1.765a2 2 0 011.082-1.083C9.602 3 10.068 3 11 3h2c.932 0 1.398 0 1.765.152a2 2 0 011.083 1.083C16 4.602 16 5.068 16 6m0 0h2m0 0h2"
-                                                        ></path>
-                                                    </g>
-                                                </svg>
-                                            </button>
-                                        </td>
-                                    </tr>
+                                                    <svg
+                                                        xmlns="http://www.w3.org/2000/svg"
+                                                        width="25"
+                                                        height="25"
+                                                        fill="none"
+                                                        viewBox="0 0 24 24"
+                                                    >
+                                                        <g>
+                                                            <path
+                                                                stroke="#000"
+                                                                strokeLinecap="round"
+                                                                strokeLinejoin="round"
+                                                                strokeWidth="2"
+                                                                d="M14 10v7m-4-7v7M6 6v11.8c0 1.12 0 1.68.218 2.108a2 2 0 00.874.874c.427.218.987.218 2.105.218h5.606c1.118 0 1.677 0 2.104-.218.377-.192.683-.498.875-.874.218-.428.218-.987.218-2.105V6M6 6h2M6 6H4m4 0h8M8 6c0-.932 0-1.398.152-1.765a2 2 0 011.082-1.083C9.602 3 10.068 3 11 3h2c.932 0 1.398 0 1.765.152a2 2 0 011.083 1.083C16 4.602 16 5.068 16 6m0 0h2m0 0h2"
+                                                            ></path>
+                                                        </g>
+                                                    </svg>
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    )}
                                 </tbody>
                             </table>
                             <div className="flex flex-col items-center px-5 py-5 xs:flex-row xs:justify-between">
---
+++ logistica/src/app/models/Order.ts
@@ -0,0 +1,8 @@
+export type Order = [{
+    id: number,
+    nome: string,
+    origem: string,
+    destino: string,
+    status: string,
+    codigoRastreio: string,
+}]
