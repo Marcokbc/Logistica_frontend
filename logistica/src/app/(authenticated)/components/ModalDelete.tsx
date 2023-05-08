8490fbaf1cc2060dc35713a1871fd5088f064ae2
Marco Antônio Meira de Lima <88397083+Marcokbc@users.noreply.github.com>
2023-05-08 10:26:49 -0300

    Delete

--- logistica/src/app/(authenticated)/components/ModalDelete.tsx
+++ logistica/src/app/(authenticated)/components/ModalDelete.tsx
@@ -15,14 +15,11 @@ export default function ModalDelete({ isVisible, onClose, pedidoId }: any) {
 
     async function deleteOrder() {
         try {
-            // alert(pedidoId);
             await api.delete(`api/Pedido/${pedidoId}`, authorization);
-            onClose;
-        }catch(error){
+        } catch (error) {
             alert(error);
-            onClose;
         }
-        
+
     }
 
     return (
@@ -77,7 +74,7 @@ export default function ModalDelete({ isVisible, onClose, pedidoId }: any) {
                             data-modal-hide="popup-modal"
                             type="button"
                             className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2"
-                            onClick={() => deleteOrder()}
+                            onClick={() => {deleteOrder(); return onClose()}}
                         >
                             Yes, I'm sure
                         </button>
